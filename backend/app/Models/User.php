7453cd1cc9152fe64e3bd4e80b77a0113e9cabe3
Halazv2 <halaziani2003@gmail.com>
2023-06-13 13:12:01 +0100

    update the model

--- backend/app/Models/User.php
+++ backend/app/Models/User.php
@@ -12,6 +12,8 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $quarded = [];
+
     /**
      * The attributes that are mass assignable.
      *
@@ -34,15 +36,5 @@ class User extends Authenticatable
     protected $hidden = [
         'password',
         'remember_token',
-    ];
-
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
-    protected $casts = [
-        'email_verified_at' => 'datetime',
-        'password' => 'hashed',
     ];
 }
