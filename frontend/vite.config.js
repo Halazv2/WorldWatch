bf55bf51899156f7f7f68a5296f49333c88d0fac
Halazv2 <halaziani2003@gmail.com>
2023-06-13 15:41:37 +0100

    change port of vite

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -10,10 +10,10 @@ export default defineConfig({
   },
   server: {
     host: true,
-    // port: 3000, // This is the port which we will use in docker
-    // // add the next lines if you're using windows and hot reload doesn't work
-    //  watch: {
-    //    usePolling: true
-    //  }
+    port: 3000, // This is the port which we will use in docker
+    // add the next lines if you're using windows and hot reload doesn't work
+     watch: {
+       usePolling: true
+     }
   }
 });
