288d0c437ef70fe217c4d6d1f284bce9ceaa0b02
Halazv2 <halaziani2003@gmail.com>
2023-06-16 01:20:56 +0100

    search for news based on selected source

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -2,6 +2,7 @@ import { configureStore } from '@reduxjs/toolkit';
 import { apiAuthSlice } from './api/apiAuthSlice';
 import { apiNewsSlice } from './api/apiNewsSlice';
 import { apiNewYorkTimesSlice } from './api/apiNewYorkTimesSlice';
+import { apiGNewsSlice } from './api/apiGNewsSlice';
 import authSlice from './modules/authSlice';
 import newsSlice from './modules/newsSlice';
 
@@ -10,11 +11,12 @@ export const store = configureStore({
     [apiAuthSlice.reducerPath]: apiAuthSlice.reducer,
     [apiNewsSlice.reducerPath]: apiNewsSlice.reducer,
     [apiNewYorkTimesSlice.reducerPath]: apiNewYorkTimesSlice.reducer,
+    [apiGNewsSlice.reducerPath]: apiGNewsSlice.reducer,
 
     auth: authSlice,
     news: newsSlice
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware({ serializableCheck: false }).concat(apiAuthSlice.middleware, apiNewsSlice.middleware, apiNewYorkTimesSlice.middleware),
+    getDefaultMiddleware({ serializableCheck: false }).concat(apiAuthSlice.middleware, apiNewsSlice.middleware, apiNewYorkTimesSlice.middleware, apiGNewsSlice.middleware),
   devTools: true
 });
