51a6e7ad4a3a3358d757bc94fa313f649a421d0e
Halazv2 <halaziani2003@gmail.com>
2023-06-16 09:38:12 +0100

    set user profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -16,11 +16,6 @@ export default function Profile() {
             <dt className="text-sm font-medium leading-6 text-gray-900">Full name</dt>
             <dd className="mt-1 flex text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
               <span className="flex-grow">{user.full_name}</span>
-              <span className="ml-4 flex-shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
             </dd>
           </div>
           <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
@@ -29,37 +24,6 @@ export default function Profile() {
               <span className="flex-grow">
                 <a href={`mailto:${user.email}`}>{user.email}</a>
               </span>
-              <span className="ml-4 flex-shrink-0">
-                <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                  Update
-                </button>
-              </span>
-            </dd>
-          </div>
-          <div className="px-4 py-6 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0">
-            <dt className="text-sm font-medium leading-6 text-gray-900">Attachments</dt>
-            <dd className="mt-1 text-sm leading-6 text-gray-700 sm:col-span-2 sm:mt-0">
-              <ul role="list" className="divide-y divide-gray-100 rounded-md border border-gray-200">
-                <li className="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
-                  <div className="flex w-0 flex-1 items-center">
-                    <PaperClipIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
-                    <div className="ml-4 flex min-w-0 flex-1 gap-2">
-                      <span className="truncate font-medium">resume_back_end_developer.pdf</span>
-                    </div>
-                  </div>
-                  <div className="ml-4 flex flex-shrink-0 space-x-4">
-                    <button type="button" className="rounded-md bg-white font-medium text-indigo-600 hover:text-indigo-500">
-                      Update
-                    </button>
-                    <span className="text-gray-200" aria-hidden="true">
-                      |
-                    </span>
-                    <button type="button" className="rounded-md bg-white font-medium text-gray-900 hover:text-gray-800">
-                      Remove
-                    </button>
-                  </div>
-                </li>
-              </ul>
             </dd>
           </div>
         </dl>
