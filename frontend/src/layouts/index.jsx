861fa8a847b6e2788be45b3cb9af5c9387ede26b
Halazv2 <halaziani2003@gmail.com>
2024-03-18 15:54:42 +0000

    Fix import case in Layout component

--- frontend/src/layouts/index.jsx
+++ frontend/src/layouts/index.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/prop-types */
-import NavBar from './navbar';
+import NavBar from './Navbar';
 import Footer from './Footer';
 
 export default function Layout({ children }) {
